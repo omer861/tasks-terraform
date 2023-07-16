4c4081bc1f40e8810eefeb2e649b7ec81eaa19dd
omer <omersyed861@gmail.com>
2023-07-16 08:40:57 +0530

    utilized data source

--- main.tf
+++ main.tf
@@ -1,42 +1,33 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-
-  required_version = ">= 1.2.0"
-}
-
 resource "aws_vpc" "myvpc" {
-  cidr_block = "10.0.0.0/16"
-  
-   tags = {
-    Name = "myvpc"
-   }
-}
 
-resource "aws_subnet" "publicsubnet" {
-  count = 2
-  vpc_id     = aws_vpc.myvpc.id
-  cidr_block = var.public_subnet_cidr[count.index]
-  availability_zone = var.availibility_zone[count.index]
+ cidr_block = "10.0.0.0/16"
+
   tags = {
-    Name = "publicsubnet"
+    Name = "myvpc"
   }
 }
 
-resource "aws_subnet" "privatesubnet" {
+resource "aws_subnet" "public_subnet" {
   count = 2
-  vpc_id = aws_vpc.myvpc.id
-  cidr_block = var.private_subnet_cidr[count.index]
-   availability_zone = var.availibility_zone[count.index]
-  
-   tags = {
-     Name = "privatesubnet"
-   }
+  vpc_id  = aws_vpc.myvpc.id
+  cidr_block = var.public_subnet_cidr[count.index]
+  availability_zone =  var.availibility_zone[count.index]
+  tags = {
+    Name = "public subnet"
+  }
 }
+
+
+resource "aws_subnet" "private_subnet" {
+  count              = 2
+  vpc_id             = aws_vpc.myvpc.id
+  cidr_block         = var.private_subnet_cidr[count.index]
+  availability_zone  = var.availibility_zone[count.index]
+  tags = {
+    Name = "private subnet"
+  }
+}
+
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.myvpc.id
@@ -46,39 +37,44 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
-resource "aws_eip" "my_eip" {
-  count = 2
+resource "aws_eip" "eip" {
+  
+  count = length(aws_subnet.public_subnet)
   vpc = true
+  
 }
 
 resource "aws_nat_gateway" "nat" {
   count         = 2  
-  allocation_id = aws_eip.my_eip[count.index].id
-  subnet_id     = aws_subnet.privatesubnet[count.index].id
+  allocation_id = aws_eip.eip[count.index].id
+  subnet_id     = aws_subnet.private_subnet[count.index].id
   
   tags = {
     Name = "nat"
   }
 }
 
-resource "aws_route_table" "rtpublic" {
+
+resource "aws_route_table" "rt_public" {
   vpc_id = aws_vpc.myvpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
+
+  tags = {
+    Name = "rt_public"
+  }
 }
 
 resource "aws_route_table_association" "rta_public" {
-  count = length(aws_subnet.publicsubnet)
-  
-  subnet_id      = aws_subnet.publicsubnet[count.index].id
-  route_table_id = aws_route_table.rtpublic.id
+  count = 2
+  subnet_id      = aws_subnet.public_subnet[count.index].id
+  route_table_id = aws_route_table.rt_public.id
 }
 
-
-resource "aws_route_table" "rtprivate" {
+resource "aws_route_table" "rt_private" {
   vpc_id = aws_vpc.myvpc.id
   count = 2
   route {
@@ -86,11 +82,18 @@ resource "aws_route_table" "rtprivate" {
     nat_gateway_id = aws_nat_gateway.nat[count.index].id
   }
 }
+  
+
 
 resource "aws_route_table_association" "rta_private" {
-    count =length(aws_subnet.privatesubnet)
-    subnet_id = aws_subnet.privatesubnet[count.index].id
-    route_table_id = aws_route_table.rtprivate[count.index].id
+  count = length(aws_subnet.private_subnet)
+  subnet_id      = aws_subnet.private_subnet[count.index].id
+  route_table_id = aws_route_table.rt_private[count.index].id
 }
+
+
+
+
+
 
 
