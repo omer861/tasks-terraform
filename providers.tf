bb05f8b8d3569ef3036de19b6e7739afa60af835
omer861 <122231964+omer861@users.noreply.github.com>
2023-07-23 14:38:33 +0530

    Terraformstate.tf (#3)
    * used user data and deployed a game
    
    * create a vpc cidr variable
    
    * resolved a internet connectivity
    
    * used s3 backend

--- providers.tf
+++ providers.tf
@@ -8,3 +8,11 @@ terraform {
 
   required_version = ">= 1.2.0"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "terraformbuck861"
+    key    = "tasks-terraform/terraform.tfstate"
+    region = "ap-south-1"
+  }
+}
