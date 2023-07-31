5ef11f8aafd8537dea016f2529bbf3043079e7eb
omer861 <122231964+omer861@users.noreply.github.com>
2023-07-31 18:36:17 +0530

    deleted files

--- userdata.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-yum install httpd -y
-service httpd start
-chkconfig httpd on
-cd /var/www/html
-ip_address=$(curl -s http://checkip.amazonaws.com)
-echo " your server ip address; $ip_address " > index.html
-service httpd restart
